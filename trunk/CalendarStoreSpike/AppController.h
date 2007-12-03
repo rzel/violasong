7f32285d057c38f5fd13a7847d430f8e8ff00bac
violasong <violasong@58385acd-3040-0410-89d3-8f96484ccad0>
2007-12-03 19:07:28 +0000

    [NEW] CalendarStoreSpike.

---
+++ trunk/CalendarStoreSpike/AppController.h
@@ -0,0 +1,16 @@
+//
+//  AppController.h
+//  CalendarStoreSpike
+//
+//  Created by wolf on 12/3/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AppController : NSObject {
+
+}
+
+@end
