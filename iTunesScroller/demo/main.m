7dc640af105e4c0eb4a20c91cc5efa2c7a91a9f5
rentzsch <rentzsch@58385acd-3040-0410-89d3-8f96484ccad0>
2008-01-13 23:18:25 +0000

    [CHANGE] iTunesScroller: upgrade to 10.5 SDK for NSGradient.

--- iTunesScroller/demo/main.m
+++ iTunesScroller/demo/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  iTunesScrollerDemo
-//
-//  Created by wolf on 1/13/08.
-//  Copyright __MyCompanyName__ 2008. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
